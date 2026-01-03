56e6ea3fecce1858c8726a4fac30c538292f5846
ars <nurkasymovarsen06@gmail.com>
2026-01-03 23:59:40 +0500

    add client.ts

--- restoMobile/api/client.ts
+++ restoMobile/api/client.ts
@@ -1,3 +1,10 @@
 import axios from "axios"
 import { API_CONFIG } from "@/constants/api.constants"
 
+export const api = axios.create({
+    baseURL: API_CONFIG.BASE_URL,
+    timeout: API_CONFIG.TIMEOUT,
+    headers: {
+        "Content-Type": "application/json"
+    }  
+})
