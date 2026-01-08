a7a62bce0318e9f4cf4625314ac4731280dd8a50
ars <nurkasymovarsen06@gmail.com>
2026-01-08 10:59:22 +0500

    feat: add updateMenuById with category validation

--- backend/src/controllers/menu.controller.js
+++ backend/src/controllers/menu.controller.js
@@ -1,4 +1,24 @@
 import Menu from '../models/menu.model'
+import Category from '../models/category.model';
+export const createMenu = async (req, res) => {
+    try {
+        const { name, description, price, category, image } = req.body;
+        if (!name || !price) {
+            return res.status(400).json({message: "Bad request"})
+        }
+        const categoryExists = await Category.findById(category);
+        if (!categoryExists) {
+            return res.status(400).json({message: "Category does not exist"})
+        }
+        const newMenu = await Menu.create({
+            name, description, price, category, image
+        })
+        const fullMenuInfo = await newMenu.populate("category", "name image"); 
+        return res.status(201).json(fullMenuInfo);    } catch (error) {
+        console.error("Error in createMenu controller", error.message);
+        return res.status(500).json({message: "Internal server error"})
+    }
+}
 
 export const getAllMenu = async (req, res) => {
     try {
@@ -7,10 +27,34 @@ export const getAllMenu = async (req, res) => {
         if (category) {
             filter.category = category
         }
-        const menuItems = await Menu.find(filter).populate('category')
+        const menuItems = await Menu.find(filter).populate('category', 'name image').select("-__v")
         return res.status(200).json(menuItems)
     } catch (error) {
         console.error("Error in getAllMenu", error.message);
+        return res.status(500).json({message: "Internal server error"})
+    }
+}
+
+export const updateMenuById = async (req, res) => {
+    try {
+        const menuId = req.params.id;
+        if (req.body.category){
+            const categoryExists = await Category.findById(req.body.category);
+            if (!categoryExists) {
+                return res.status(400).json({message: "Bad request. Category does not exist"})
+            }        
+        } 
+
+        const updatedMenu = await Menu.findByIdAndUpdate(menuId, req.body, {
+            new: true,
+            runValidators: true
+        })
+        if (!updatedMenu) {
+            return res.status(404).json({message: "Menu not found"})
+        }
+        return res.status(200).json(updatedMenu)
+    } catch (error) {
+        console.error("Error in updateMenuById controller")
         return res.status(500).json({message: "Internal server error"})
     }
 }
