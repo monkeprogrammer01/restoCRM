ce266cf76ed93a06d74d05f15f1ab9002eab773e
ars <nurkasymovarsen06@gmail.com>
2026-01-09 22:59:22 +0500

    feat: add useMenu.ts

---
+++ restoMobile/hooks/useMenu.ts
@@ -0,0 +1,35 @@
+import { useState } from "react"
+import { menuService } from "@/services/menu.service";
+import { Dish } from "@/types/menu.types";
+export const useMenu = async () => {
+    const [loading, setLoading] = useState(false);
+    const [dishes, setDishes] = useState<Dish[]>([]);
+    const fetchDishes = async () => {
+        try {
+            setLoading(true)
+            const response = await menuService.getDishes();
+            setDishes(response);
+        } catch (error) {
+            console.error("Error in fetchDishes", error)
+
+            throw error
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    const fetchDishesByCategory = async (category: string) => {
+        try {
+            setLoading(true);
+            const response = await menuService.getDishesByCategory(category);
+            setDishes(response)
+        } catch (error) {
+            console.error("Error in fetchDishesByCategory", error)
+            throw error
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    return {fetchDishes, dishes, loading}
+}
